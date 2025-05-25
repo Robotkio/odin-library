6239583ef0bdbb6a34460b7e013b821797d23323
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-05-24 23:46:01 -0700

    added basic elements and the styling for them

--- library.js
+++ library.js
@@ -1,1 +1,13 @@
 console.log("Hello world!");
+
+const myLibrary = [];
+
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToKibrary(book) {
+    myLibrary.push(book);
+}
